1a744ec148601dca34d7cb4cf37f2f90599015cc
Anjali <markandeanjali@gmail.com>
2023-05-18 00:01:43 +0530

    Add existing project files to Git

---
+++ Tutorial.js
@@ -0,0 +1,30 @@
+var firstName = "Devi";
+var lastName = 'Dewangan';
+var middleName = `Purnima`;
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(middleName));
+
+// dynamic type conversion
+
+var a ="Purnima";
+console.log("Line no 14",typeof a);
+a = 1233;
+console.log("Line no 16"+ typeof a);
+// var a = 34564;
+var a = 4.321;
+console.log("Line no 18 Redeclare variable", typeof a);
+
+// Interview Asked Questiona
+
+console.log("typeof Positive =>",typeof Infinity);
+console.log("typeof Negative =>",typeof -Infinity);
+
+var check = true;
+var checker = typeof check;
+console.log(typeof checker);
+
+
